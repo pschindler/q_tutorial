02820a827c1d56650a2b07689042f244f5314c07
Philipp Schindler <philipp.schindler@uibk.ac.at>
2018-01-16 21:30:05 +0100

    small additions to tutorial

--- citizen_q.py
+++ citizen_q.py
@@ -30,3 +30,12 @@ def draw_bloch(vector):
     sphere.vector_color = ['r']
     sphere.add_vectors(vector)
     sphere.show()
+def check_fidelity(psi90):
+    target_90 = rx(pi/2)*ground_state()
+    f = fidelity(psi90,target_90)**2
+    print ("Your fidelity is {}".format(f))
+    if f > 0.99:
+        print("You have successfully finished the first tutorial")
+    else:
+        print("Your fidelity did not reach the threshold")
+    
